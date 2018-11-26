ad7688daae6e5612e651272a7c7d955474d7157d
tower1229 <tower1229@foxmail.com>
2018-11-26 19:10:33 +0800

    update

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,24 @@
 import axios from 'axios';
-import qs from 'qs';
 import * as util from '../assets/util.js';
 
-export const baseURL = '/';
+const URLHASH = {
+  mock: 'http://rap2api.taobao.org/app/mock/3567',
+  dev: 'http://view.sooc.com',
+  test: ''
+};
 
-const instance = axios.create({
-  baseURL: baseURL,
+export const baseURL = URLHASH.mock;
+
+export const instance = axios.create({
+  baseURL,
   timeout: 10000,
   headers: {
-    'Content-Type':'application/x-www-form-urlencoded'
+    'Content-Type':'application/json'
   }
 });
 
 // 请求处理
 instance.interceptors.request.use(function (config) {
-  if(config.data){
-    config.data = qs.stringify(config.data)
-  }
   return config;
 }, function (error) {
   return Promise.reject(error);
