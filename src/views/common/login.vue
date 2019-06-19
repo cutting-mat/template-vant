c61f64a42d5e5622b134cabfe9111e77fca7eca9
tower1229 <tower1229@foxmail.com>
2019-06-19 14:59:33 +0800

    update

--- src/views/common/login.vue
+++ src/views/common/login.vue
@@ -1,7 +1,37 @@
 <template>
-<div style="display: table;height:100%;width:100%">
-	<div class="g-center">
-		<h1 style="text-align:center">login</h1>
-	</div>
-</div>
+  <div>
+    <h1 class="title">login</h1>
+    <form class="myform">
+      <div class="_item">
+        <cube-input v-model="form.username" placeholder="用户名"></cube-input>
+      </div>
+      <div class="_item">
+        <cube-input type="password" v-model="form.password" placeholder="密码"></cube-input>
+      </div>
+      <cube-button primary type="submit">登录</cube-button>
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.title{font-size:1.5em;margin: 2rem 0;text-align: center;}
+.myform {
+  padding: 1rem;
+}
+.myform ._item {
+  margin-bottom: 0.5rem;
+}
+</style>
